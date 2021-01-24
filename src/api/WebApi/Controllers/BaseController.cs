c8965abc713bb7905a79466eab3cb56ec957f361
Grzegorz Sawiński <PLU11555@PLD01040>
2021-01-24 13:50:32 +0100

    Add compose and WebApi

---
+++ src/api/WebApi/Controllers/BaseController.cs
@@ -0,0 +1,34 @@
+﻿using CSharpFunctionalExtensions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected new IActionResult Ok()
+        {
+            return base.Ok(Envelope.Ok());
+        }
+
+        protected IActionResult Ok<T>(T result)
+        {
+            return base.Ok(Envelope.Ok(result));
+        }
+
+        protected IActionResult Error(string errorMessage)
+        {
+            return BadRequest(Envelope.Error(errorMessage));
+        }
+
+        protected IActionResult FromResult(Result result)
+        {
+            return result.IsSuccess 
+                ? Ok() 
+                : Error(result.Error);
+        }
+    }
+}
