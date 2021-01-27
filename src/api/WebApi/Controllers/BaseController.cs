1ae16eb830745bb2fa96c2c83c2c7d1263def2d1
Grzegorz Sawiński <PLU11555@PLD01040>
2021-01-27 18:46:33 +0100

    Fix api

--- src/api/WebApi/Controllers/BaseController.cs
+++ src/api/WebApi/Controllers/BaseController.cs
@@ -1,9 +1,6 @@
 ﻿using CSharpFunctionalExtensions;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using WebApi.Utils;
 
 namespace WebApi.Controllers
 {
--- src/api/WebApi/Utils/HandlerRegistration.cs
+++ src/api/WebApi/Utils/HandlerRegistration.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using Logic.Models;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Api.Utils
